4787daaacb1fb5d474f64e8bf3d499d82afd3d3f
DilumFernando <dilumamilafdo1995@gmail.com>
2024-09-19 19:51:27 -0400

    Added the test files after testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using InsertionSort
 using Test
 
-@testset "InsertionSort" begin
-    @test check_sort(100) == true
+@testset "Insertion sort tests" begin
+    @test insertion_sort!([3, 2, 1]) == [1, 2, 3]
+    @test insertion_sort!([3, 2, 1, 4]) == [1, 2, 3, 4]
 end
